ef50a43b269218f36ba8048385cf755128d269e9
ivanmula <ivanmula74@gmail.com>
2026-03-09 18:03:04 +0800

    Updated

--- print_smallest_number.py
+++ print_smallest_number.py
@@ -3,4 +3,7 @@ num2 = (float(input('Enter second number: ')))
 
 if num1 < num2:
     print(f"The smallest number is {num1}")
-
+if num2 < num1:
+    print(f"The smallest number is {num2}")
+else:
+    print("The numbers are equal")
