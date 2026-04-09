35c2b6796be8ebe7c6cbef1ce207631ba968f6c2
ivanmula <ivanmula74@gmail.com>
2026-04-10 02:12:34 +0800

    Add logic to capitalize string without capitalize()

--- custom_capitalize.py
+++ custom_capitalize.py
@@ -1,1 +1,14 @@
 s = input("Enter string: ")
+if s:
+    first = s[0]
+    rest = s[1:]
+    if "a" <= first <= "z":
+        first = chr(ord(first) - 32)
+    new_rest = ""
+    for c in rest:
+        if "A" <= c <= "Z":
+            new_rest += chr(ord(c) + 32)
+        else:
+            new_rest += c
+    s = first + new_rest
+print(s)
