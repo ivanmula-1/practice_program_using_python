8a834f1ab590b781b65923d6a1bacea98f7235ec
ivanmula <ivanmula74@gmail.com>
2026-04-10 02:10:04 +0800

    Add logic to center string without center()

--- custom_center.py
+++ custom_center.py
@@ -1,2 +1,8 @@
 s = input("Enter string: ")
 width = int(input("Enter total width: "))
+if len(s) < width:
+    total_space = width - len(s)
+    left_space = total_space // 2
+    right_space = total_space - left_space
+    s = " " * left_space + s + " " * right_space
+print(f"'{s}'")
