684a5619e3c1fd7aef0804ebfa3545e58e56f85e
ivanmula <ivanmula74@gmail.com>
2026-04-10 00:56:45 +0800

    Add logic to display numbers with duplicates

--- numbers_with_duplicates.py
+++ numbers_with_duplicates.py
@@ -3,3 +3,6 @@ numbers = []
 for i in range(10):
     num = int(input(f"Enter number {i+1}: "))
     numbers.append(num)
+duplicates = [n for n in numbers if numbers.count(n) > 1]
+duplicates = list(dict.fromkeys(duplicates))
+print("Numbers with duplicates:", duplicates)
