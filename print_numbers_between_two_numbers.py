e87558c7728d49de36ab6622e6f202099f9263f2
ivanmula <ivanmula74@gmail.com>
2026-04-09 23:45:01 +0800

    Complete program for printing numbers between two inputs

--- print_numbers_between_two_numbers.py
+++ print_numbers_between_two_numbers.py
@@ -1,2 +1,8 @@
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
+
+start = min(num1, num2)
+end = max(num1, num2)
+
+for num in range(start + 1, end):
+    print(num)
