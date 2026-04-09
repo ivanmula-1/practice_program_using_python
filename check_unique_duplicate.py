e8ca80912848b07e23de7a699dac0e5081998961
ivanmula <ivanmula74@gmail.com>
2026-04-10 00:45:56 +0800

    Add logic to check if number is unique or duplicate and handle input

--- check_unique_duplicate.py
+++ check_unique_duplicate.py
@@ -1,1 +1,13 @@
 numbers = []
+
+while True:
+    try:
+        num = int(input("Enter a number: "))
+    except ValueError:
+        break
+
+    if num in numbers:
+        print("Duplicate")
+    else:
+        print("Unique")
+        numbers.append(num)
