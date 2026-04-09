afcce4f8749ec371736d889b0fd6cf1d1555df15
ivanmula <ivanmula74@gmail.com>
2026-04-10 02:07:34 +0800

    Add logic to check string endswith without endswith()

--- custom_endswith.py
+++ custom_endswith.py
@@ -1,2 +1,6 @@
 custom_endswith.pys = input("Enter string: ")
 suffix = input("Enter suffix: ")
+if s[-len(suffix):] == suffix:
+    print(True)
+else:
+    print(False)
--- custom_isupper.py
+++ custom_isupper.py
@@ -1,3 +1,2 @@
 s = input("Enter string: ")
-is_upper = all(not c.isalpha() or "A" <= c <= "Z" for c in s)
-print(is_upper)
+suffix = input("Enter suffix: ")
