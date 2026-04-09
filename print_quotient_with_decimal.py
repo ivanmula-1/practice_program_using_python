574346c507e8fa2c9eae0970a7ec9291c97c95f3
ivanmula <ivanmula74@gmail.com>
2026-04-09 23:59:21 +0800

    Complete program for printing the quotient with decimal

--- print_quotient_with_decimal.py
+++ print_quotient_with_decimal.py
@@ -1,2 +1,7 @@
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
+
+if num2 != 0:
+    print("Quotient:", num1 / num2)
+else:
+    print("Cannot divide by zero")
