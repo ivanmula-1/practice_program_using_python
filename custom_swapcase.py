5d36a2abe3ff6971fdf19b6e636087f09bdd8b61
ivanmula <ivanmula74@gmail.com>
2026-04-10 02:11:02 +0800

    Add logic to swap case without swapcase()

--- custom_swapcase.py
+++ custom_swapcase.py
@@ -1,1 +1,10 @@
 s = input("Enter string: ")
+result = ""
+for c in s:
+    if "A" <= c <= "Z":
+        result += chr(ord(c) + 32)
+    elif "a" <= c <= "z":
+        result += chr(ord(c) - 32)
+    else:
+        result += c
+print(result)
