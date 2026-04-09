7b4d3b974885bc75519dfc1bedb5aff949192ccc
ivanmula <ivanmula74@gmail.com>
2026-04-10 00:44:31 +0800

    Add logic to display numbers skipping duplicates

--- numbers_skip_duplicates.py
+++ numbers_skip_duplicates.py
@@ -3,3 +3,8 @@ numbers = []
 for i in range(10):
     num = int(input(f"Enter number {i+1}: "))
     numbers.append(num)
+    seen = []
+    for n in numbers:
+        if n not in seen:
+            print(n)
+            seen.append(n)
