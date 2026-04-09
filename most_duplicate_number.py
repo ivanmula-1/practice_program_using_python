b2e5935b5a9a630f53703b05783e56649b8d6cf3
ivanmula <ivanmula74@gmail.com>
2026-04-10 00:59:58 +0800

    Add logic to find and display number with most duplicates

--- most_duplicate_number.py
+++ most_duplicate_number.py
@@ -6,4 +6,7 @@ while True:
         numbers.append(num)
     except ValueError:
         break
+if numbers:
+    most_dup = max(set(numbers), key=numbers.count)
+    print("Number with most duplicates:", most_dup)
 
