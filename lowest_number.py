684dc2b49448f349f64c35c97231914e6613a4ee
ivanmula <ivanmula74@gmail.com>
2026-04-10 00:49:39 +0800

    Add logic to check if number is unique or duplicate and handle input

--- lowest_number.py
+++ lowest_number.py
@@ -6,3 +6,14 @@ while True:
         numbers.append(num)
     except ValueError:
         break
+while True:
+    try:
+        num = int(input("Enter a number: "))
+    except ValueError:
+        break
+
+    if num in numbers:
+        print("Duplicate")
+    else:
+        print("Unique")
+        numbers.append(num)
